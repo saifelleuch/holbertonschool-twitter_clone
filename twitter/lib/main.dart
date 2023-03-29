245767123ed3385556ca8cc5e4dd157351133aa6
saifelleuch <sa2014if@gmail.com>
2023-03-29 20:27:21 +0100

    MaterialApp to SignIn()

--- twitter/lib/main.dart
+++ twitter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/signin_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      home: SignIn(),
     );
   }
 }
